7dbff1012823b162680c28ef23d8bc577c3799df
Hajai-J <hajaignc@gmail.com>
2024-06-25 17:56:31 +0530

    small change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+   return 'Hello World Azure'
 
 if __name__ == '__main__':
-   app.run(debug=True, host="0.0.0.0", port=8080)
+   app.run(debug=True, host="0.0.0.0", port=5000)
